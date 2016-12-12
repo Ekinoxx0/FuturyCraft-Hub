e7ed0931dfddcb39e0bbafa015c92ddfad702fe9
loucass003 <loucass003@gmail.com>
2016-12-12 22:36:08 +0100

    Fix API queueManager

--- src/fc/hub/Main.java
+++ src/fc/hub/Main.java
@@ -1,7 +1,35 @@
 package fc.hub;
 
+import api.API;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
 /**
  * Created by loucass003 on 12/12/16.
  */
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin
+{
+    public static Main instance;
+    public API api;
+
+    public Main()
+    {
+        instance = this;
+        api = API.getInstance();
+        api.useQueueManager(false);
+    }
+
+    @Override
+    public void onEnable()
+    {
+        getLogger().log(Level.INFO, "Enabled !");
+    }
+
+    @Override
+    public void onDisable()
+    {
+        getLogger().log(Level.INFO, "Disabled !");
+    }
+
 }
